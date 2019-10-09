f1424b21c30fe7c200412bb0f6f41d366a76164a
dxzo <keng8684@outlook.es>
2019-10-08 21:22:43 -0500

    code ref

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const ROOT = "http://localhost:3000/auth";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,10 +11,7 @@ export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
   AuthCredentials(username: string, password: string, callback: Function) {
-    //console.log(userName, password);
-    return this.http.post("http://localhost:3000/auth", {
-      username, password
-    }).subscribe(
+    return this.http.post(ROOT, { username, password }).subscribe(
       (value) => callback(value), 
       (err) => callback(err)
     );
